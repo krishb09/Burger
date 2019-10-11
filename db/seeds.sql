3a64252dc151854d52161258f14c1299917cf629
Krish <aalsbhowmick@gmail.com>
2019-10-10 20:52:27 -0400

    added css/imgs/form

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Chicken Burger", false); 
+INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES (" Hamburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES (" Cheeseburger", false);
